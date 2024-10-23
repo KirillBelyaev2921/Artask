e5ccf4984805a86b609a51535b4799a12c1501ef
KirillBelyaev2921 <kirillbelyaev2921@gmail.com>
2024-10-23 20:24:46 +0300

    chore: init.

---
+++ artask/src/main/java/ua/arthead/artask/ArtaskApplication.java
@@ -0,0 +1,13 @@
+package ua.arthead.artask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtaskApplication.class, args);
+	}
+
+}
