06e398ca9652a7fab099ac0fcfbe71c47ad8b8d8
KirillBelyaev2921 <kirillbelyaev2921@gmail.com>
2024-11-18 14:17:11 +0200

    refactor: change tasks view.

--- artask/src/main/java/ua/arthead/artask/ArtaskApplication.java
+++ artask/src/main/java/ua/arthead/artask/ArtaskApplication.java
@@ -1,5 +1,6 @@
 package ua.arthead.artask;
 
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,13 @@ public class ArtaskApplication {
 
 	@Bean
 	public CommandLineRunner commandLineRunner(TaskRepository repository) {
-		return args -> repository.save(new Task("Home"));
+		return args -> {
+			Task home = new Task("Home");
+			Task task1 = new Task("Task 1", home);
+			Task task2 = new Task("Task 2", home);
+			Task task3 = new Task("Task 3", task1);
+			Task task4 = new Task("Task 4", task1);
+			repository.saveAll(List.of(home, task1, task2, task3, task4));
+		};
 	}
 }
