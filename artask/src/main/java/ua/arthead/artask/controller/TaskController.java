75ed7316ee552074e79294c444de3560bdfb17ca
KirillBelyaev2921 <kirillbelyaev2921@gmail.com>
2024-11-26 15:58:53 +0200

    feat: add task adding feature.

--- artask/src/main/java/ua/arthead/artask/controller/TaskController.java
+++ artask/src/main/java/ua/arthead/artask/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.arthead.artask.data.model.Task;
 import ua.arthead.artask.service.TaskService;
 
@@ -25,9 +26,21 @@ public class TaskController {
     return "tasks";
   }
 
-  @PostMapping("/task")
-  public String addTaskPage(@ModelAttribute("task") Task task) {
+
+  @GetMapping("/addTask")
+  public String addTaskPage(@RequestParam(required = false) Long id, Model model) {
+    Task parentTask = taskService.getTaskById(id == null ? 1 : id).orElseThrow();
+    Task newTask = new Task();
+    newTask.setParentTask(parentTask);
+    model.addAttribute("newTask", newTask);
     return "task";
+  }
+
+  @PostMapping("/task")
+  public String addTask(@ModelAttribute("task") Task task) {
+    task.setParentTask(taskService.getTaskById(task.getParentTask().getId()).orElseThrow());
+    taskService.saveTask(task);
+    return "redirect:/tasks";
   }
 
   @DeleteMapping("/task")
